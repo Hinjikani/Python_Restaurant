6fe0fd93d7d83cb6c1caab98a696d2a81dc5001a
Hinjikani <pascalhanif@gmail.com>
2024-11-05 23:22:47 +0700

    changing to os.system("cls") for windows

--- restaurant.py
+++ restaurant.py
@@ -47,38 +47,38 @@ def showMenu():
             case _:
                 print("\033[31mhmmm, i'm sorry but i don't think the item exist in our menu. Please input the menu number or name\033[0m")
                 time.sleep(3)
-                os.system("clear")
+                os.system("cls")
                 continue
 
 def more():
     global showingMenu
     while showingMenu:
-        os.system("clear")
+        os.system("cls")
         showOrders()
         more = input("Do you want to order anything else? (y/n): ")
         more.lower()
         match more:
             case "y":
                 showingMenu = True
-                os.system("clear")
+                os.system("cls")
                 break
             case "n":
                 showingMenu = False
-                os.system("clear")
+                os.system("cls")
                 break
             case _:
-                os.system("clear")
+                os.system("cls")
                 print("\033[31mDo you want to order anything else or what??? >:(\033[0m")
                 time.sleep(2)
                 continue
 
 def showOrders():
     if orders.count("Ice Cream") > 0:
-        print(f"Ice Cream x {orders.count("Ice Cream")}: {menu["Ice Cream"] * orders.count("Ice Cream")}")
+        print(f"Ice Cream x {orders.count('Ice Cream')}: {menu['Ice Cream'] * orders.count('Ice Cream')}")
     if orders.count("Crepes") > 0:
-        print(f"Crepes x {orders.count("Crepes")}: {menu["Crepes"] * orders.count("Crepes")}")
+        print(f"Crepes x {orders.count('Crepes')}: {menu['Crepes'] * orders.count('Crepes')}")
     if orders.count("Froyo") > 0:
-        print(f"Froyo x {orders.count("Froyo")}: {menu["Froyo"] * orders.count("Froyo")}")
+        print(f"Froyo x {orders.count('Froyo')}: {menu['Froyo'] * orders.count('Froyo')}")
     iceCreamTotal = menu["Ice Cream"] * orders.count("Ice Cream")
     crepesTotal = menu["Crepes"] * orders.count("Crepes")
     froyoTotal = menu["Froyo"] * orders.count("Froyo")
@@ -92,11 +92,7 @@ menu = {
 
 orders = []
 
-iceCreamCount = orders.count("Ice Cream")
-CrepesCount = orders.count
-
 #Main Code
-
 print("Welcome to BlaBla Ice Cream Shop!!!")
 time.sleep(2)
 
